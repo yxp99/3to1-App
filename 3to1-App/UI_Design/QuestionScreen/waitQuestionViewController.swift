136ecb550e84deed1232b8e7985ee3e12bae2e6e
yxp99 <31594400+yxp99@users.noreply.github.com>
2019-01-13 11:15:19 -0500

    Update waitQuestionViewController.swift

--- 3to1-App/UI_Design/QuestionScreen/waitQuestionViewController.swift
+++ 3to1-App/UI_Design/QuestionScreen/waitQuestionViewController.swift
@@ -30,7 +30,31 @@ class waitQuestionViewController: UIViewController {
         /*  Set up Navigation Bar 3to1 logo
         This method is in ViewControllerHelper.swift */
         customizeNavigationBar()
-        
+        - (void)revealController:(SWRevealViewController *)revealController willAddViewController:(UIViewController *)viewController
+    forOperation:(SWRevealControllerOperation)operation animated:(BOOL)animated;
+- (void)revealController:(SWRevealViewController *)revealController didAddViewController:(UIViewController *)viewController
+    forOperation:(SWRevealControllerOperation)operation animated:(BOOL)animated;
+
+// Support for custom transition animations while replacing child controllers. If implemented, it will be fired in response
+// to calls to 'setXXViewController' methods
+- (id<UIViewControllerAnimatedTransitioning>)revealController:(SWRevealViewController *)revealController
+    animationControllerForOperation:(SWRevealControllerOperation)operation fromViewController:(UIViewController *)fromVC toViewController:(UIViewController *)toVC;
+
+// DEPRECATED - The following delegate methods will be removed some time in the future
+- (void)revealController:(SWRevealViewController *)revealController panGestureBeganFromLocation:(CGFloat)location progress:(CGFloat)progress; // (DEPRECATED)
+- (void)revealController:(SWRevealViewController *)revealController panGestureMovedToLocation:(CGFloat)location progress:(CGFloat)progress; // (DEPRECATED)
+- (void)revealController:(SWRevealViewController *)revealController panGestureEndedToLocation:(CGFloat)location progress:(CGFloat)progress; // (DEPRECATED)
+@end
+
+
+#pragma mark - UIViewController(SWRevealViewController) Category
+
+// A category of UIViewController to let childViewControllers easily access their parent SWRevealViewController
+@interface UIViewController(SWRevealViewController)
+
+- (SWRevealViewController*)revealViewController;
+
+@end
         /*  Changes the background of the main view.
         This method is in addBackground.swift  */
         self.view.addBackground(imageName: "Background.png")
